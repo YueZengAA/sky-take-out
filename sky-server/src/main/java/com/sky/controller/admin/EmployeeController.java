0cc469535ce774eab1c763512b798dd6e32ef119
Yue Zeng <tianazengyue@outlook.com>
2025-05-13 03:57:30 -0400

    分类管理代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -43,7 +43,7 @@ public class EmployeeController {
     @PostMapping("/login")
     @ApiOperation(value = "员工登录")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
-        log.info("员工登录：{}", employeeLoginDTO);
+        //log.info("员工登录：{}", employeeLoginDTO);
 
         Employee employee = employeeService.login(employeeLoginDTO);
 
@@ -79,7 +79,7 @@ public class EmployeeController {
     @PostMapping
     @ApiOperation("新增员工")
     public Result save(@RequestBody EmployeeDTO employeeDTO) {
-        log.info("新增员工, {}", employeeDTO);
+        //log.info("新增员工, {}", employeeDTO);
         System.out.println("当前线程id：" + Thread.currentThread().getId());
         employeeService.save(employeeDTO);
         return Result.success();
@@ -88,7 +88,7 @@ public class EmployeeController {
     @GetMapping("/page")
     @ApiOperation("员工分页查询")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
-        log.info("员工分页查询，参数为： {}", employeePageQueryDTO);
+        //log.info("员工分页查询，参数为： {}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
@@ -102,7 +102,7 @@ public class EmployeeController {
     @PostMapping("/status/{status}")
     @ApiOperation("启用/禁用员工账号")
     public Result startOrStop(@PathVariable Integer status, Long id) {
-        log.info("启用/禁用员工账号：{}, {}", status, id);
+        //log.info("启用/禁用员工账号：{}, {}", status, id);
         employeeService.startOrStop(status, id);
         return Result.success();
     }
@@ -127,7 +127,7 @@ public class EmployeeController {
     @PutMapping
     @ApiOperation("编辑员工信息")
     public Result update(@RequestBody EmployeeDTO employeeDTO) {
-        log.info("编辑员工信息: {}", employeeDTO);
+        //log.info("编辑员工信息: {}", employeeDTO);
         employeeService.update(employeeDTO);
         return Result.success();
     }

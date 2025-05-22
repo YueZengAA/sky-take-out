ccfac9a64adb7b8fb4a7fa1d26d4380765c68971
Yue Zeng <tianazengyue@outlook.com>
2025-05-22 18:22:12 +0800

    店铺营业状态设置代码实现

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,41 @@
+package com.sky.controller.admin;
+
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/shop")
+@Api(tags="店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "shop_status";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺营业状态")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺营业状态为：{}", status == 1 ? "open" : "close");
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("店铺营业状态为： {}", status == 1 ? "open" : "close");
+        return Result.success(status);
+    }
+
+
+
+}
